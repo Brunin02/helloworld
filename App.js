239d1933179cc3279e51d89be4955a26d416c032
Bruno2b <leal.silva.bruno@escola.pr.gov.br>
2023-04-03 14:43:46 -0300

    teste

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CCCCFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
